4d141bdffe4a07feb0b525e34e9b2d548451133e
Kenichi Nishiya <yorimitsu@proton.me>
2023-09-26 22:05:37 -0300

    Update config files

--- scripts/git.sh
+++ scripts/git.sh
@@ -24,6 +24,7 @@ cp $IN'picom/picom.conf' $OUT'picom/picom.conf'
 # cp $IN'nvim/lua/custom/plugins.lua' $OUT'nvim/plugins/plugins.lua'
 cp $IN'tmux/tmux.conf' $OUT'tmux/tmux.conf'
 cp $IN'sway/config' $OUT'sway/config'
+cp -r $IN'ytdl' $OUT'ytdl'
 
 # cp ~/Documents/suse.sh $REPO 
 # cp ~/.vimrc $REPO'vimrc'
