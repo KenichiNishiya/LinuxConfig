3081308b2e71286a28210aec8d94724a1df062ad
Kenichi Nishiya <yorimitsu@proton.me>
2023-11-23 21:04:27 -0300

    Update config files

---
+++ scripts/stu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# export PATH=/usr/bin:/bin:/usr/local/bin
+# export GIT_SSH_COMMAND="ssh -i /home/yori/.config/tk/tk"
+cd /home/yori/study
+git add .
+git commit -m 'Upload files'
+git push origin main
+
