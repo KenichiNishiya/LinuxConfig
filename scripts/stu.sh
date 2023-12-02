cadde982ec5f7e9361d35b2bf08419f6e9799c13
Kenichi Nishiya <yorimitsu@proton.me>
2023-12-02 13:38:04 -0300

    Update config files

--- scripts/stu.sh
+++ scripts/stu.sh
@@ -2,6 +2,7 @@
 # export PATH=/usr/bin:/bin:/usr/local/bin
 # export GIT_SSH_COMMAND="ssh -i /home/yori/.config/tk/tk"
 cd /home/yori/study
+git pull
 git add .
 git commit -m 'Upload files'
 git push origin main
