9eecb2c44d918506221e0ded2357699739ad7f81
KenichiNishiya <yorimitsu@proton.me>
2024-01-23 22:25:52 -0300

    Update config files

--- scripts/averagetemp.sh
+++ scripts/averagetemp.sh
@@ -1,19 +1,64 @@
 #!/bin/bash
 
 COUNTER=1
+MAX=0
+MAX15=0
+MIN=0
+MIN15=0
+SEC=${1:-1}
+
+R='\033[0;31m'
+G='\033[0;32m'
+Y='\033[1;33m'
+N='\033[0m'
+
+DOTS=""
+
 while true; do
-    if [ $COUNTER == 14 ];then
+    if [ $COUNTER == 16 ];then
         COUNTER=1
         TEMP=0
+        MAX15=0
+        MIN15=0
+        DOTS=""
     fi
+
     NEWTEMP=$(sensors | grep Tctl | awk '{print $2}' | cut -d '+' -f 2 | cut -d "." -f 1)
     TEMP=$(( $TEMP+$NEWTEMP ))
     AVERAGE=$(( $TEMP/$COUNTER ))
-    echo "Average CPU temperature: "$AVERAGE 
+
+    if [ $MAX == 0 ] || [ $MAX -lt $NEWTEMP ]; then
+        MAX=$NEWTEMP
+    fi
+    if [ $MIN == 0 ] || [ $MIN -gt $NEWTEMP ]; then
+        MIN=$NEWTEMP
+    fi
+
+    if [ $MAX15 == 0 ] || [ $MAX15 -lt $NEWTEMP ]; then
+        MAX15=$NEWTEMP
+    fi
+    if [ $MIN15 == 0 ] || [ $MIN15 -gt $NEWTEMP ]; then
+        MIN15=$NEWTEMP
+    fi
+    DOTS+="."
+
+    clear
+    echo -e "\033[0;36m$DOTS${N}"
+    echo -e "+--------------+"
+    echo -e "|${Y}    Temp  $NEWTEMP  ${N}|"
+    echo -e "|${Y} Avg 15s  $AVERAGE  ${N}|"
+    echo -e "+--------------+"
+    echo -e "|${R}     Max  $MAX  ${N}|"
+    echo -e "|${R} Max 15s  $MAX15  ${N}|"
+    echo -e "+--------------+"
+    echo -e "|${G}     Min  $MIN  ${N}|"
+    echo -e "|${G} Min 15s  $MIN15  ${N}|"
+    echo -e "+--------------+"
+
     # echo "TEMP: "$TEMP
     # echo "NEWTEMP: "$NEWTEMP
     # echo "COUNTER: "$COUNTER
     COUNTER=$(( $COUNTER+1 ))
-    sleep 1
+    sleep $SEC
 done
 
